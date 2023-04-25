eb5f14b27ec59790294b4bc4fdc4ac3867053b37
SohailaFarouk <sohailafarouk88@gmail.com>
2023-04-25 16:47:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+	/**
+	 * main - print _putchar
+	 * is written by sohaila
+	 * Return: 0
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
